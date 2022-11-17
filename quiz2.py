4e43231f19ca8e6d4db7b2fc58e658b1f759bf64
Mark Rattle <mark.rattle@gmail.com>
2022-11-17 12:50:47 -0500

    first commit

---
+++ quiz2.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, render_template
+from PIL import Image, ImageFilter
+from pprint import PrettyPrinter
+from dotenv import load_dotenv
+import json
+import os
+import random
+import requests
+
+load_dotenv()
+
+app = Flask(__name__)
