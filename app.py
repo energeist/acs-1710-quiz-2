7cedf1fce6a16efde45d73bbe37857c7fdae3219
Mark Rattle <mark.rattle@gmail.com>
2022-11-18 21:23:45 -0500

    all done

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import requests
 
 app = Flask(__name__)
 
-SWAPI_BASE_URL = 'https://swapi.py4e.com/api/'
 SWAPI_PEOPLE_API = 'https://swapi.py4e.com/api/people/'
 
 @app.route('/', methods=['GET', 'POST'])
@@ -16,18 +15,14 @@ def swapi_search():
     SWAPI_PEOPLE_API = 'https://swapi.py4e.com/api/people/'
     if request.method == 'POST':
         index = request.form.get("index")
-        SWAPI_PEOPLE_API = 'https://swapi.py4e.com/api/people/' + index
-        
+        SWAPI_PEOPLE_API = 'https://swapi.py4e.com/api/people/' + index  
         try:            
             response = requests.get(SWAPI_PEOPLE_API)
-  
         except KeyError:
             context = {
                 'detail': 'Not found'
             }
-
-        if json.loads(response.content).get('detail') == 'Not found':
-            
+        if json.loads(response.content).get('detail') == 'Not found':        
             # give 'no data exists' API repsonse to render 404
             context = {
                 'detail': 'Not found'
@@ -44,15 +39,13 @@ def swapi_search():
             }
             films_list = json.loads(response.content).get('films')
             print(films_list)
-            
             # homeworld search 
             try:
                 homeworld_response = requests.get(json.loads(response.content).get('homeworld'))
                 homeworld = json.loads(homeworld_response.content).get('name')
                 context['homeworld'] = homeworld
             except KeyError:
-                context['homeworld'] = ''
-            
+                context['homeworld'] = ''           
             # movie search
             movie_titles = []
             try:
@@ -63,7 +56,6 @@ def swapi_search():
                 context['movies'] = movie_titles
             except KeyError:
                 context['movies'] = ''
-
         return render_template('swapi_search.html', **context)
     else:
         return render_template('swapi_search.html')
